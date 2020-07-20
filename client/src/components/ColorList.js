e529b6309221d2afc19ce1c8d0e9c79e7360ad43
stephanie mairena <stefyem7711@gmail.com>
2020-07-19 21:28:05 -0400

    stuckonsave

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-//import axios from "axios";
+import axiosWithAuth from "../axiosWithAuth";
+
 
 const initialColor = {
   color: "",
@@ -16,15 +17,27 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
+
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is it saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colors.id}`, colorToEdit)
+    .then(res => {
+     setColorToEdit(res.data)
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/123`)
+      .then(res => {
+        updateColors(res)
+      })
+      .catch(err => console.log(err))
   };
 
   return (
